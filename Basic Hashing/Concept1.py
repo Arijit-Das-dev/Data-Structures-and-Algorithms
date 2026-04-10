7f8abb09b810aa557727d9aa9f794c7fd3e17154
Arijit Das <arijitdas.das99@gmail.com>
2026-04-10 21:59:37 +0530

    Basic Hashing

---
+++ Basic Hashing/Concept1.py
@@ -0,0 +1,14 @@
+# calculate occurrence of each integer number in an array
+
+arr = [1, 1, 1, 2, 4, 2, 4, 5, 6, 5]
+
+hashMap = {}
+
+for i in arr:
+    if i not in hashMap:
+        hashMap[i] = 1
+    else:
+        hashMap[i] += 1
+
+for x, y in hashMap.items():
+    print(f"Number {x} occurred {y} times")
