a8439f9207aebbac6b7ef97ea1842f13ffd7b96d
Arijit Das <arijitdas.das99@gmail.com>
2026-04-23 15:28:25 +0530

    Arrays/Sorting_Algorithm/Selection_Sort

--- Arrays/Sorting_Algorithm/Selection_Sort/Discending_Order.py
+++ Arrays/Sorting_Algorithm/Selection_Sort/Discending_Order.py
@@ -1,3 +1,19 @@
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 n = len(nums)
+
+for i in range(0, n):
+
+    max_num = i
+
+    for j in range(i+1, n):
+
+        if nums[j] > nums[max_num]:
+
+            max_num = j
+
+    temp = nums[i]
+    nums[i] = nums[max_num]
+    nums[max_num] = temp
+
+print(nums)
