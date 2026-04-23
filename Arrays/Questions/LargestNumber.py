14544294a19b35200e13c4ef6c1ec95b75dad73f
Arijit Das <arijitdas.das99@gmail.com>
2026-04-23 13:30:02 +0530

    updated

--- Arrays/Questions/LargestNumber.py
+++ Arrays/Questions/LargestNumber.py
@@ -7,7 +7,20 @@ largest = float('-inf')
 
 for i in range(0, n):
 
-    if nums[i] > largest:
+    if (nums[i] > largest):
         largest = nums[i]
 
 print(largest)
+
+# approach 2
+nums = [1, 2, 3, 4, 5]
+
+n = len(nums)
+largest_num = nums[0]
+
+for i in range(1, n):
+
+    if (nums[i] > largest_num):
+        largest_num = nums[i]
+
+print(largest_num)
