07924655a79674303555a80360ad1c5f16ecfff8
Arijit Das <arijitdas.das99@gmail.com>
2026-04-24 17:59:37 +0530

    updated

--- Arrays/Questions/SecondLargestNumber.py
+++ Arrays/Questions/SecondLargestNumber.py
@@ -1,22 +1,27 @@
-# Approach 1
-nums = [1, 2, 3, 4, 5]
+"""
+Question : First and Second largest number in an array
 
-n = len(nums)
+"""
 
-first_largest = float('-inf')
-second_largest = float('-inf')
+class Solution(object):
 
-for i in range(0, n):
+    def firstAndSecondLargestNumber(self, nums):
 
-    if (nums[i] > first_largest):
-        first_largest = nums[i]
+        n = len(nums)
+        first_largest = float('-inf')
+        second_largest = float('-inf')
 
-print(f"First largest number in {nums}: {first_largest}")
+        for i in range(0, n):
 
+            if (nums[i]>first_largest):
+                first_largest = nums[i]
 
-for j in range(0, n):
+        for j in range(0, n):
 
-    if (nums[j] > second_largest) & (nums[j] != first_largest):
-        second_largest = nums[j]
-
-print(f"Second largest number in {nums} : {second_largest}")
+            if (nums[j]>second_largest) & (nums[j] != first_largest):
+                second_largest = nums[j]
+        
+        return first_largest, second_largest
+    
+s = Solution()
+print(s.firstAndSecondLargestNumber(nums = [52, -1, 100, 150, 1200, -120.0]))
