9e36dd90f1784bf67ca3e5c346fa653a2fde7d5e
Arijit Das <arijitdas.das99@gmail.com>
2026-04-24 21:45:38 +0530

    updated

---
+++ Arrays/Questions/checkArraySorted.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    
+    def check(self, nums):
+
+        n = len(nums)
+        for i in range(0, n-1):
+            if nums[i] > nums[i+1]:
+                return False
+        return True
+    
+
+# Example usage:
+s = Solution()
+print(s.check([1, 2, 3, 4, 5, 6]))
+print(s.check([10, 9, 8, 7, 6, 5]))
