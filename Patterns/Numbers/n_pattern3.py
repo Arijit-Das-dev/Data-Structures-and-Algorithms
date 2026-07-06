d442931ba22394c21fb941b067b8c966f28f203c
Arijit Das <arijitdas.das99@gmail.com>
2026-07-06 18:01:29 +0530

    updated

---
+++ Patterns/Numbers/n_pattern3.py
@@ -0,0 +1,16 @@
+# Expected output
+"""
+1
+1 2
+1 2 3 
+1 2 3 4
+1 2 3 4 5
+
+row (i) -> 5
+column (j) -> 5
+"""
+
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
