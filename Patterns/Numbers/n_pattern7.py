595b4a617307a771b514eb39be5d3361c62099b8
Arijit Das <arijitdas.das99@gmail.com>
2026-07-07 19:09:19 +0530

    updated

--- Patterns/Numbers/n_pattern7.py
+++ Patterns/Numbers/n_pattern7.py
@@ -22,3 +22,9 @@ for i in range(5, 0, -1):
         print(i, end=" ")
     num = num + 1
     print()
+
+# method 3
+for i in range(5, 0, -1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
