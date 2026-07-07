3f9781793da265d16fc62557ca8430ac15d13023
Arijit Das <arijitdas.das99@gmail.com>
2026-07-07 18:46:55 +0530

    updated

---
+++ Patterns/Numbers/n_pattern7.py
@@ -0,0 +1,18 @@
+# Expected output
+""" 
+5 5 5 5 5
+4 4 4 4
+3 3 3 
+2 2
+1
+"""
+
+# method 1
+num = 5
+for i in range(1, 6):
+    for j in range(1, 7-i):
+        print(num, end=" ")
+    num = num - 1
+    print()
+
+# method 2
