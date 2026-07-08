34dacee66a7288318bad28fa3080e2c7a3c34148
Arijit Das <arijitdas.das99@gmail.com>
2026-07-08 21:41:21 +0530

    updated

--- Basic_Questions/Strings/questions.py
+++ Basic_Questions/Strings/questions.py
@@ -12,5 +12,13 @@
 
 # Reverse a string.
 # method 1
-string = "arijit"
-print(string[::-1])
+string1 = "arijit"
+print(string1[::-1])
+
+# method 2
+string2 = "arijit"
+newstring = ""    
+
+for i in range(len(string2)-1, -1, -1):
+    newstring += string2[i]
+print(newstring)
