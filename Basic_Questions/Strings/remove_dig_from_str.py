ef1ad544d1fb89dd9be7f402927c33b9b85370d5
Arijit Das <arijitdas.das99@gmail.com>
2026-07-08 22:48:50 +0530

    updated

--- Basic_Questions/Strings/remove_dig_from_str.py
+++ Basic_Questions/Strings/remove_dig_from_str.py
@@ -1,1 +1,14 @@
 # Remove all digits from a string.
+string = "abcdefghijklmnopqrstuvwxyz12345678910"
+
+new_arr = []
+nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+for char in string:
+    if char not in nums:
+        new_arr.append(char)
+    else:
+        continue
+
+strings = "".join(new_arr)
+print(strings)
