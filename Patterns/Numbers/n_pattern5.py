7b8fc216f005eb2a9d45478bbedb5f6a5cbeeb83
Arijit Das <arijitdas.das99@gmail.com>
2026-07-08 13:11:13 +0530

    updated

--- Patterns/Numbers/n_pattern5.py
+++ Patterns/Numbers/n_pattern5.py
@@ -8,7 +8,16 @@
 row (i) -> 4
 column (j) -> 4
 """
-for i in range(1, 10):
-    for j in range(i, 10):
+
+# method 1
+for i in range(1, 5):
+    for j in range(i, 5):
+        print("*", end=" ")
+    print()
+
+
+# method 2
+for i in range(1, 5):
+    for j in range(1, 6-i):
         print("*", end=" ")
     print()
